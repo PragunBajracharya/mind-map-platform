86d3ca4a295b3189a286a731087b66107958e873
PragunBajracharya <pragunbaj99@gmail.com>
2024-09-26 20:39:51 +0545

    fix: ts-node

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,25 +2,23 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 import userRoutes from "./routes/userRoutes";
-import authRoutes from "./routes/authRoutes";
-import mindmapRoutes from "./routes/mindmapRoutes";
+// import authRoutes from "./routes/authRoutes";
+// import mindmapRoutes from "./routes/mindmapRoutes";
 import getRouteList from "./utils/getRouteList";
 
 dotenv.config();
 
-const app:Express = express();
-connectDB();
+const app: Express = express();
+// connectDB();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/mindmaps", mindmapRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/mindmaps", mindmapRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-console.log(getRouteList(app));
-
-
+// console.log(getRouteList(app));
